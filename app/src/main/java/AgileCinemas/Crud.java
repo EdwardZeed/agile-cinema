4e4c4fa7ba61472ff49395ef9db6dc90220f8719
Edward Zheng <edward@Edwards-MacBook-Pro.local>
2021-10-21 00:15:03 +1100

    update what i've done for retrieveing data to display movies

--- app/src/main/java/AgileCinemas/Crud.java
+++ app/src/main/java/AgileCinemas/Crud.java
@@ -1,7 +1,11 @@
 package AgileCinemas;
 
-import java.sql.PreparedStatement;
+import java.sql.*;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 
 public class Crud {
     // All static methods RETURN AT LEAST A BOOLEAN (FOR SUCCESS)
@@ -22,8 +26,38 @@ public class Crud {
     public static ArrayList<MovieViewing> getViewings() {
         // Access database
         ArrayList<MovieViewing> sessions = new ArrayList<>();
-        String sql = "select MOVIE_ID from VIEWINGS join MOVIES on (MOVIE_ID = ID) group by MOVIE_ID";
-//        PreparedStatement ps =
+        // get current date
+        SimpleDateFormat format = new SimpleDateFormat("YYYY-MM-dd");
+        Date date = new Date(System.currentTimeMillis());
+        String currentDate = format.format(date);
+        // get date after a week
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(date);
+        calendar.add(Calendar.DAY_OF_YEAR, 7);
+        Date dateAfterWeek = calendar.getTime();
+        String dateAfterWeekString = format.format(dateAfterWeek);
+
+        Connection conn = null;
+        try {
+            conn = DriverManager.getConnection("jdbc:sqlserver://soft2412.cyg3iolyiokd.ap-southeast-2.rds.amazonaws.com:1433;", "admin", "gr0up!wo");
+            String sql = "select MOVIE_ID from VIEWINGS join MOVIES on (MOVIE_ID = ID) where Date between ? and ? group by MOVIE_ID";
+            PreparedStatement ps = conn.prepareStatement(sql);
+            ps.setString(1, currentDate);
+            ps.setString(2, dateAfterWeekString);
+
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()){
+                // need to double-check the exact column number
+                Movie movie = new Movie(rs.getInt(10), rs.getString(11), rs.getString(12), rs.getString(13), rs.getString(14), rs.getString(15), rs.getString(16));
+                MovieViewing movieViewing = new MovieViewing(movie);
+                sessions.add(movieViewing);
+            }
+            conn.close();
+
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+
         return null;
     }
 
@@ -35,6 +69,7 @@ public class Crud {
     public static boolean checkPasswordExist(String password){
         return password.equals("1508");
     }
+
 
 
 
